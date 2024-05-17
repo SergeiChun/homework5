e261f36d8b48d5fa861396784c36a7c32cd8db5b
SergeiChun <GunneRX@yandex.ru>
2024-05-17 20:14:30 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,15 @@
+my_list = ['appel', 'pear', 'banana', 'orange','lemon','tangerine',
+'coconut','kiwi', 'pineapple']
+print(my_list)
+print(my_list[0])
+print (my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'chery'
+print(my_list)
+
+
+my_dict = {'parrot':'попугай','floor':'пол', 'pig':'cвинья','salt':'соль'}
+print(my_dict)
+print(my_dict['floor'])
+my_dict['chair'] = 'стул'
+print(my_dict)
